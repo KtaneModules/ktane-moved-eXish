e99b3ca05c20e04157d341412623a5056409c468
Brendan King <brendan@brendanking.com>
2023-10-11 16:12:26 -0400

    Removed rule seed and ability to press question mark

--- Assets/SimpleModuleScript.cs
+++ Assets/SimpleModuleScript.cs
@@ -12,9 +12,7 @@ public class SimpleModuleScript : MonoBehaviour {
 	public KMAudio audio;
 	public KMBombInfo info;
 	public KMBombModule module;
-	public KMRuleSeedable ruleSeed;
 	public KMSelectable[] movingButton;
-	public KMSelectable[] pressableModule;
 	public GameObject movingButtonObject;
 	static int ModuleIdCounter = 1;
 	int ModuleId;
@@ -27,7 +25,6 @@ public class SimpleModuleScript : MonoBehaviour {
 
 	bool _isSolved = false;
 	bool incorrect = false;
-	int ruleSelected;
 
 	void Awake()
 	{
@@ -38,12 +35,6 @@ public class SimpleModuleScript : MonoBehaviour {
 			KMSelectable pressedButton = button;
 			button.OnInteract += delegate () { movingButtonStuff(pressedButton); return false; };
 		}
-
-		foreach (KMSelectable button in pressableModule)
-		{
-			KMSelectable pressedButton = button;
-			button.OnInteract += delegate () { ModulePressing(pressedButton); return false; };
-		}
 	}
 
 	void Start()
@@ -53,16 +44,11 @@ public class SimpleModuleScript : MonoBehaviour {
 		ypos = Rnd.Range (0f, 2f / 10f);
 
 		movingButtonObject.transform.localPosition = new Vector3 (xpos, ypos, zpos);
-
-		var rng = ruleSeed.GetRNG();
-		Log("Using rule seed: " + rng.Seed);
-		if (rng.Seed != 1)
-			ruleSelected = rng.Next(3);
 	}
 
 	void movingButtonStuff(KMSelectable pressedButton)
 	{
-		audio.PlayGameSoundAtTransformWithRef(KMSoundOverride.SoundEffect.ButtonPress, transform);
+		audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, pressedButton.transform);
 		int buttonPosition = new int();
 		for(int i = 0; i < movingButton.Length; i++)
 		{
@@ -75,8 +61,6 @@ public class SimpleModuleScript : MonoBehaviour {
 
 		if (_isSolved == false) 
 		{
-			if (ruleSelected == 1)
-				incorrect = true;
 			switch (buttonPosition) 
 			{
 			case 0:
@@ -85,45 +69,6 @@ public class SimpleModuleScript : MonoBehaviour {
 			}
 			if (incorrect) 
 			{
-				Log("However, I did not want you to press the button. Strike!");
-				module.HandleStrike ();
-				incorrect = false;
-			}
-			else
-			{
-				correct.Play ();
-				module.HandlePass();
-				_isSolved = true;
-			}
-		}
-	}
-
-	void ModulePressing(KMSelectable pressedButton)
-	{
-		audio.PlayGameSoundAtTransformWithRef(KMSoundOverride.SoundEffect.ButtonPress, transform);
-		int buttonPosition = new int();
-		for(int i = 0; i < movingButton.Length; i++)
-		{
-			if (pressedButton == movingButton[i])
-			{
-				buttonPosition = i;
-				break;
-			}
-		}
-
-		if (_isSolved == false) 
-		{
-			if (ruleSelected == 2)
-				incorrect = true;
-			switch (buttonPosition) 
-			{
-			case 0:
-				Log ("Ok cool.");
-				break;
-			}
-			if (incorrect) 
-			{
-				Log("However, I did not want you to press the question mark. Strike!");
 				module.HandleStrike ();
 				incorrect = false;
 			}
@@ -144,7 +89,7 @@ public class SimpleModuleScript : MonoBehaviour {
 	//Twitch Plays support
 
 	#pragma warning disable 414
-	private readonly string TwitchHelpMessage = @"!{0} button/b [Presses the button] | !{0} ? [Presses the question mark]";
+	private readonly string TwitchHelpMessage = @"!{0} button/b [Presses the button]";
 	#pragma warning restore 414
 
 	IEnumerator ProcessTwitchCommand(string command)
@@ -153,11 +98,6 @@ public class SimpleModuleScript : MonoBehaviour {
 		{
 			yield return null;
 			movingButton[0].OnInteract();
-		}
-		else if (command.EqualsIgnoreCase("?"))
-		{
-			yield return null;
-			pressableModule[0].OnInteract();
 		}
 	}
 
